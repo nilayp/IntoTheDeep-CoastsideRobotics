f0cf5616da45e7c9ed3004679d1d959424accc48
Nilay Patel <nilayp@gmail.com>
2024-10-16 12:29:01 -0700

    Add TeleOp classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleOpArcade.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Simple TeleOp Arcade", group="Robot")
+public class SimpleTeleOpArcade extends OpMode {
+
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+
+    double driveSpeed;
+    double leftPower;
+    double turnPower;
+
+    public void init() {
+        backLeftDrive = hardwareMap.dcMotor.get("back_left_motor");
+        backRightDrive = hardwareMap.dcMotor.get("back_right_motor");
+
+        // reverses the left motor because it is mounted upside down
+       backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // starts the robot at full speed
+        driveSpeed = 1.0;
+
+    }
+    public void loop() {
+        leftPower = -gamepad1.left_stick_y;
+        turnPower = gamepad1.right_stick_x;
+
+        // toggles the drive speed between 0.5 and 1.0
+        // by pressing the left bumper
+        if (gamepad1.left_bumper) {
+            if (driveSpeed == 0.5) {
+                driveSpeed = 1.0;
+            } else {
+                driveSpeed = 0.5;
+            }
+        }
+        arcadeDrive(leftPower, turnPower, driveSpeed);
+        updateTelemetry();
+    }
+    public void arcadeDrive(double forwardPower, double turnPower, double driveSpeed) {
+        double leftPower = forwardPower + turnPower;
+        double rightPower = forwardPower - turnPower;
+
+        // scales the power of the motors if the maximum power is greater than 1.0
+        // by taking the maximum of the absolute values of the left and right powers
+
+        double max = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+        if (max > 1.0) {
+            leftPower = leftPower / max;
+            rightPower = rightPower / max;
+        }
+        backLeftDrive.setPower(leftPower * driveSpeed);
+        backRightDrive.setPower(rightPower * driveSpeed);
+    }
+    public void updateTelemetry() {
+        telemetry.addData("GamePad1 left_stick_y", leftPower);
+        telemetry.addData("GamePad1 right_stick_x", turnPower);
+        telemetry.addData("Drive Speed", driveSpeed);
+        telemetry.addData("Left Power", backLeftDrive.getPower());
+        telemetry.addData("Right Power", backRightDrive.getPower());
+        telemetry.update();
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleOpTank.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Simple TeleOp Tank", group="Robot")
+public class SimpleTeleOpTank extends OpMode {
+
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+
+    double driveSpeed;
+    double leftPower;
+    double rightPower;
+
+    public void init() {
+        backLeftDrive = hardwareMap.dcMotor.get("back_left_motor");
+        backRightDrive = hardwareMap.dcMotor.get("back_right_motor");
+
+        // reverses the left motor because it is mounted upside down
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // starts the robot at full speed
+        driveSpeed = 1.0;
+
+    }
+    public void loop() {
+        leftPower = -gamepad1.left_stick_y;
+        rightPower = -gamepad1.right_stick_y;
+
+        // toggles the drive speed between 0.5 and 1.0
+        // by pressing the left bumper
+        if (gamepad1.left_bumper) {
+            if (driveSpeed == 0.5) {
+                driveSpeed = 1.0;
+            } else {
+                driveSpeed = 0.5;
+            }
+        }
+        tankDrive(leftPower, rightPower, driveSpeed);
+        updateTelemetry();
+    }
+    public void tankDrive(double leftPower, double rightPower, double driveSpeed) {
+        backLeftDrive.setPower(leftPower * driveSpeed);
+        backRightDrive.setPower(rightPower * driveSpeed);
+    }
+    public void updateTelemetry() {
+        telemetry.addData("GamePad1 left_stick_y", leftPower);
+        telemetry.addData("GamePad1 right_stick_y", rightPower);
+        telemetry.addData("Drive Speed", driveSpeed);
+        telemetry.addData("Left Power", backLeftDrive.getPower());
+        telemetry.addData("Right Power", backRightDrive.getPower());
+        telemetry.update();
+    }
+}
